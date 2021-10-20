f896d55bbf15faa9f2787a451c78565b059bbc58
luo <rockyluo@163.com>
2021-10-20 10:01:28 +0800

    learn table sql

--- src/main/scala/com/atguigu/day7/TableExample.scala
+++ src/main/scala/com/atguigu/day7/TableExample.scala
@@ -33,6 +33,30 @@ object TableExample {
           .field("timestamp", DataTypes.BIGINT())
           .field("temperature", DataTypes.DOUBLE())
       ).createTemporaryTable("inputTable")  // 创建临时表
+    // 初始化环境
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    // 初始化环境配置，创建planner
+    val settings: EnvironmentSettings = EnvironmentSettings
+      .newInstance()
+      .useBlinkPlanner()  // 使用blink planner， 流批一体
+      .inStreamingMode()
+      .build()
+
+    // 初始化表环境
+    val tEnv = StreamTableEnvironment.create(env, settings)
+
+    // 扫描注册订单表
+    tEnv.connect(new FileSystem().path("/Users/carves/Documents/projects/flink_learn_scala/src/main/resources/sensor.txt"))
+      .withFormat(new Csv())
+      .withSchema(
+        new Schema()
+          .field("id", DataTypes.STRING())
+          .field("timestamp", DataTypes.BIGINT())
+          .field("temperature", DataTypes.DOUBLE())
+      ).createTemporaryTable("inputTable")  // 创建临时表
+
 
 
     // 将临时表转换成Table
@@ -46,7 +70,7 @@ object TableExample {
     tEnv.toAppendStream[Row](result).print()
 
 //    // 使用sql api查询
-val sqlResult = tEnv.sqlQuery("select id, temperature from inputTable where id = 'sensor_1'")
+  val sqlResult = tEnv.sqlQuery("select id, temperature from inputTable where id = 'sensor_1'")
 
     tEnv.toAppendStream[Row](sqlResult).print()
 
